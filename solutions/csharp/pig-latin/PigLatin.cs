01b597bfd17874a11414f8c3534e1f01530dbaf5
root <root@ip-10-1-2-198.eu-west-2.compute.internal>
2025-06-06 11:20:23 +0000

    [Sync Iteration] csharp/pig-latin/1

---
+++ solutions/csharp/pig-latin/PigLatin.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+public static class PigLatin
+{
+    public static string Translate(string sentence) =>
+        String.Join(' ', sentence.Split(' ').Select(Convert));
+
+    public static string Convert(string word)
+    {
+        var basic = Regex.Match(word, @"^[aeiou]|^xr|^yt");
+        if (basic.Success)
+            return word + "ay";
+
+        var y = Regex.Match(word, @"^([^aiueo]+)(y.*)$");
+        if (y.Success)
+            return Ay(y);
+
+        var qu = Regex.Match(word, @"^([^aiueo]*qu)(.*)$");
+        if (qu.Success)
+            return Ay(qu);
+
+        var consonant = Regex.Match(word, @"^([^aiueo]+)(.*)$");
+        if (consonant.Success)
+            return Ay(consonant);
+        
+        return word;
+    }
+
+    private static string Ay(Match m) =>
+        m.Groups[2].ToString() + m.Groups[1].ToString() + "ay";
+}
