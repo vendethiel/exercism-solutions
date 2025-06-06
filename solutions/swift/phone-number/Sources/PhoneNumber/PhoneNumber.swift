9fa4078d4f7dcc7231ec9bb98e22f4d2232d1a7c
root <root@ip-10-1-2-198.eu-west-2.compute.internal>
2025-06-06 11:20:24 +0000

    [Sync Iteration] swift/phone-number/2

--- solutions/swift/phone-number/Sources/PhoneNumber/PhoneNumber.swift
+++ solutions/swift/phone-number/Sources/PhoneNumber/PhoneNumber.swift
@@ -10,30 +10,30 @@ class PhoneNumber {
     number = phoneNumber
   }
 
-  func clean() throws(PhoneNumberError) -> String {
+  func clean() throws -> String {
     if number.contains("@") {
-      throw .invalidPhoneNumber
+      throw PhoneNumberError.invalidPhoneNumber
     }
 
     var numbers = number.filter { c in 48...57 ~= c.asciiValue ?? 0 }
     switch numbers.count {
     case 11:
       if numbers.first != "1" {
-        throw .invalidPhoneNumber
+        throw PhoneNumberError.invalidPhoneNumber
       }
       numbers.removeFirst()
       fallthrough
     case 10:
       if (numbers.first == "0" || numbers.first == "1") {
-        throw .invalidPhoneNumber
+        throw PhoneNumberError.invalidPhoneNumber
       }
       let exchange = numbers[numbers.index(numbers.startIndex, offsetBy: 3)]
       if (exchange == "0" || exchange == "1") {
-        throw .invalidPhoneNumber
+        throw PhoneNumberError.invalidPhoneNumber
       }
       return numbers
     default:
-      throw .invalidPhoneNumber
+      throw PhoneNumberError.invalidPhoneNumber
     }
   }
 }
