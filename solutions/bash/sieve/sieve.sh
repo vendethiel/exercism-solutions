ad165349225170a761d6b580dacbdda5a6e7be3d
root <root@ip-10-1-2-198.eu-west-2.compute.internal>
2025-06-06 11:20:30 +0000

    [Sync Iteration] bash/sieve/2

--- solutions/bash/sieve/sieve.sh
+++ solutions/bash/sieve/sieve.sh
@@ -2,7 +2,7 @@
 UPTO="$1"
 check() {
     CUR=$1
-    for((i=2; i<=$CUR/2; i++))
+    for((i=2; i<=CUR/2; i++))
     do
       ans=$(( CUR%i ))
       if [ $ans -eq 0 ]
@@ -12,7 +12,7 @@ check() {
     done
     return 0
 }
-for ((x = 2; x <= $UPTO; x++))
+for ((x = 2; x <= UPTO; x++))
 do
     check $x && echo $x
 done | xargs
