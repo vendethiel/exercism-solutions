c68b014075363209fd33233c9b8cf2c615521d65
root <root@ip-10-1-2-198.eu-west-2.compute.internal>
2025-06-06 11:20:11 +0000

    [Sync Iteration] elixir/roman-numerals/1

---
+++ solutions/elixir/roman-numerals/lib/roman_numerals.ex
@@ -0,0 +1,38 @@
+defmodule RomanNumerals do
+  @doc """
+  Convert the number to a roman number.
+  """
+  @spec numeral(pos_integer) :: String.t()
+  def numeral(number) do
+    {_, res} = {number, ""}
+    |> triple(1000, "M")
+    |> single(900, "CM")
+    |> triple(500, "D")
+    |> single(400, "CD")
+    |> triple(100, "C")
+    |> single(90, "XC")
+    |> triple(50, "L")
+    |> single(40, "XL")
+    |> triple(10, "X")
+    |> single(9, "IX")
+    |> single(5, "V")
+    |> single(4, "IV")
+    |> triple(1, "I")
+    res
+  end
+
+  def single({number, str}, min, x) do
+    if number >= min do
+      {number - min, str <> x}
+    else
+      {number, str}
+    end
+  end
+
+  def triple(a, b, c) do
+    a
+    |> single(b, c)
+    |> single(b, c)
+    |> single(b, c)
+  end
+end
