f4c7ec9a8272de3b27048bbb716321fe33fe4dc4
root <root@ip-10-1-2-198.eu-west-2.compute.internal>
2025-06-06 11:20:23 +0000

    [Sync Iteration] csharp/protein-translation/3

--- solutions/csharp/protein-translation/ProteinTranslation.cs
+++ solutions/csharp/protein-translation/ProteinTranslation.cs
@@ -2,24 +2,23 @@ using System;
 using System.Linq;
 public static class ProteinTranslation
 {
-    public static string[] Proteins(string strand)
-    {
-        return strand
+    public static string[] Proteins(string strand) =>
+        strand
             .Chunk(3)
             .Select(x => new string(x))
             .Select(Convert)
             .TakeWhile(x => x is not null)
             .ToArray();
-    }
 
-    private static string Convert(string tri) => tri switch {
-        "AUG" => "Methionine",
-        "UUU" or "UUC" => "Phenylalanine",
-        "UUA" or "UUG" => "Leucine",
-        "UCU" or "UCC" or "UCA" or "UCG" => "Serine",
-        "UAU" or "UAC" => "Tyrosine",
-        "UGU" or "UGC" => "Cysteine",
-        "UGG" => "Tryptophan",
-        _ => null,
-    };
+    private static string Convert(string tri) =>
+        tri switch {
+            "AUG" => "Methionine",
+            "UUU" or "UUC" => "Phenylalanine",
+            "UUA" or "UUG" => "Leucine",
+            "UCU" or "UCC" or "UCA" or "UCG" => "Serine",
+            "UAU" or "UAC" => "Tyrosine",
+            "UGU" or "UGC" => "Cysteine",
+            "UGG" => "Tryptophan",
+            _ => null,
+        };
 }
