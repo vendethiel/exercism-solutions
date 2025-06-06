f4aecd200e4ddf1e79fa38fc509e226233830850
root <root@ip-10-1-2-198.eu-west-2.compute.internal>
2025-06-06 11:20:22 +0000

    [Sync Iteration] crystal/protein-translation/2

--- solutions/crystal/protein-translation/src/protein_translation.cr
+++ solutions/crystal/protein-translation/src/protein_translation.cr
@@ -18,13 +18,14 @@ module ProteinTranslation
     "UAG": "STOP",
     "UGA": "STOP",
   }
-  
+
   def self.proteins(strand : String) : Array(String)
     strand.chars.in_slices_of(3).map do |x|
       MAPPING.fetch(x.join(""), "")
     end.take_while do |x|
-      raise ArgumentError.new if x == ""
       x != "STOP"
+    end.tap do |xs|
+      raise ArgumentError.new if xs.includes?("")
     end
   end
 end
